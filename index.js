4d67ea22b366ce5e32379f5e0dc786952f64eb0a
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-24 18:43:35 +0600

    Added menu delete functionality

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
       console.log(token);
       res.send({ token });
     });
-    
+
     const verifyToken = (req, res, next) => {
       console.log("From verify token", req.headers);
       if (!req.headers.authorization) {
@@ -64,14 +64,21 @@ async function run() {
       next();
     };
 
-
     app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
     });
+
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/menu/:id",  async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -101,7 +108,6 @@ async function run() {
       const result = await usersCollection.insertOne(users);
       res.send(result);
     });
-
 
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
